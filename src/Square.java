3212d11c042928171e98981e37168ac15634bfb1
unknown <thuyle8824@gmail.com>
2020-07-10 17:28:00 +0700

    add

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,26 @@
-public class Square {
+public class Square extends Rectangle {
+    public Square() {
+        width=1.0;
+        lenght=1.0;
+    }
+    public Square (double side) {
+        super(side,side);
+    }
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color,filled);
+    }
+    public double getSide() {
+        return getWidth();
+    }
+    public void setSide(double side) {
+        setWidth(side);
+        setLenght(side);
+    }
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
 }
